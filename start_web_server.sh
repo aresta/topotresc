14ab68cdbb9ff498f507a7cf7d05e3d26459e344
gos <6231546+aresta@users.noreply.github.com>
2020-02-22 12:59:18 +0100

    fix

--- start_web_server.sh
+++ start_web_server.sh
@@ -1,8 +1,8 @@
 echo "Restarting apache"
 # docker-compose exec tileserver service apache2 stop
 # # docker-compose exec tileserver a2enconf mod_tile
-# docker-compose exec tileserver service apache2 start
-docker-compose exec tileserver service apache2 reload
+docker-compose exec tileserver service apache2 start
+docker-compose exec tileserver service apache2 reload # sometimes is needed
 
 echo "Starting renderd"
 docker-compose exec tileserver renderd -f -c /usr/local/etc/renderd.conf
