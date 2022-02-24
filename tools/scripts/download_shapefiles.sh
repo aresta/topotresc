16f297e387fdc7e8a219854e953932bf11936289
gos <6231546+aresta@users.noreply.github.com>
2022-02-24 12:45:19 +0100

    Replaced script to download base shp and unzip

--- tools/scripts/download_shapefiles.sh
+++ tools/scripts/download_shapefiles.sh
@@ -5,6 +5,14 @@
   # OpenStreetMap data file that you downloaded above, some shapefiles for
   # things like low-zoom country boundaries are still needed. To download and
   # index these:
-python /mnt/openstreetmap-carto/scripts/get-shapefiles.py -d /mnt/base_data
+
+[ -d /mnt/base_data ] || mkdir -p /mnt/base_data
 cd /mnt/base_data
+wget https://osmdata.openstreetmap.de/download/simplified-water-polygons-split-3857.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
+wget https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
+unzip '*.zip'
 rm -f *.zip
+[ -d ne_110m_admin_0_boundary_lines_land ] || mkdir -p ne_110m_admin_0_boundary_lines_land
+mv ne_110m_admin_0_boundary_lines_land.* ne_110m_admin_0_boundary_lines_land
+
